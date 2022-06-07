6cb45cc0effa00f31cfd54a0cf068dda901041bd
ziomzziom <54291606+ziomzziom@users.noreply.github.com>
2022-06-07 21:55:56 +0200

    Update packet filtring iptables.bash

--- packet filtring iptables.bash
+++ packet filtring iptables.bash
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+#Delete all existing rules
+    iptables -F
+
 #Granting access to the ports that we need
     iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-    iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-    iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
-#Allow unband traffic
-    iptables -A OUTPUT -j ACCEPT
+#Drop traffics to firewall
+    iptables -A INPUT -p tcp --dport 80 -j REJECT
+    iptables -A INPUT -p tcp --dport 443 -j REJECT
 
+#Allow inbound traffic
+#    iptables -A OUTPUT -j ACCEPT
+
+#Deny ping
+    iptables -A INPUT -p icmp -j REJECT
 #Allow ping
-    iptables -A INPUT -p icmp -j ACCEPT
+#    iptables -A INPUT -p icmp -j ACCEPT
 
-#Rewoke access from everything else
-    iptables -A INPUT -j DROP
-    iptables -A FORWARD -j DROP
+#Revoke access from everything else
+#    iptables -A INPUT -j DROP
+#    iptables -A FORWARD -j DROP
 
-#Block access from specific IP Addresses You can block total access from an IP Address using:
+#Block access from specific IP Addresses You can block total access from an IP Address using
 #    iptables -A INPUT -s IP-ADDRESS -j DROP
 #Example
 #    iptables -A INPUT -s 87.53.42.120 -j DROP
@@ -23,5 +30,3 @@
 ##How Do I Unblock An IP Address
 #    iptables -D INPUT -s xx.xxx.xx.xx -j DROP
 #    iptables -D INPUT -s 87.53.42.120 -j DROP
-
-service iptables save
