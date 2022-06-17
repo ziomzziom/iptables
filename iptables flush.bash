48058a6b300260e89995b71ac552d09bde13cdea
ziomzziom <54291606+ziomzziom@users.noreply.github.com>
2022-06-17 23:00:48 +0200

    Update iptables flush.bash

---
+++ iptables flush.bash
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "The firewall is now being closed. All policies are set to value 
+ACCEPT, all rules and chains are removed, all counters are set to
+zero value. "
+
+
+ipt="/sbin/iptables"
+
+# Setting default policies to ACCEPT
+    $ipt -p INPUT ACCEPT 
+    $ipt -p FORWARD ACCEPT 
+    $ipt -p OUTPUT ACCEPT 
+    $ipt -t nat -p OUTPUT ACCEPT 
+    $ipt -t nat -p PREROUTING ACCEPT 
+    $ipt -t nat -p POSTROUTING ACCEPT 
+    $ipt -t mangle -p INPUT ACCEPT 
+    $ipt -t mangle -p OUTPUT ACCEPT 
+    $ipt -t mangle -p FORWARD ACCEPT 
+    $ipt -t mangle -p PREROUTING ACCEPT 
+    $ipt -t mangle -p POSTROUTING ACCEPT 
+
+# Reset all counters
+    $ipt -Z 
+    $ipt -t nat -Z 
+    $ipt -t mangle -Z
+
+# Remove all rules and chains 
+    $ipt -F 
+    $ipt -X 
+    $ipt -t nat -F 
+    $ipt -t nat -X 
+    $ipt -t mangle -F 
+    $ipt -t mangle -X
